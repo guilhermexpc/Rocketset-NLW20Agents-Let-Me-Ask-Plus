9c2085cc5b1e34727ac5693b7e65572b4b8469b4
Guilherme <guilhermee1219@gmail.com>
2025-07-07 22:37:42 -0300

    Setup Inicial - API Servidor

---
+++ server/src/server.ts
@@ -0,0 +1,32 @@
+import { fastify } from "fastify";
+import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from "fastify-type-provider-zod";
+import { fastifyCors } from "@fastify/cors";
+import { env } from "./env.ts";
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.register(fastifyCors, {
+  origin: "http://localhost:5173"
+});
+
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
+
+//BKP
+// app.listen( { port: process.env.PORT ? Number(process.env.PORT) : 3333 }).then(() => {
+//   console.log(`Port: ${process.env.PORT}`)
+//   console.log("HTTP Server running! FOI WOOOOO");
+//  })
+
+// app.get("/testonildo", () => {
+//   return "OK";
+// });
+
+app.get("/health", () => {
+  return "OK";
+});
+
+app.listen({ port: env.PORT }).then(() => {
+  console.log(`Port: ${process.env.PORT}`);
+  console.log("HTTP Server running! FOI WOOOOO");
+});
